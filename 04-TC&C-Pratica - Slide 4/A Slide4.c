f44104570323397ccdb5178f45ed00f86e929011
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2023-10-26 19:51:59 -0300

    Add files via upload

---
+++ 04-TC&C-Pratica - Slide 4/A Slide4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// Função para calcular o tamanho de uma string de forma recursiva
+int recursive_length(char *str) {
+    if (*str == '\0') {
+        return 0;
+    }
+    return 1 + recursive_length(str + 1);
+}
+
+int main() {
+    char filename[] = "seuarquivo.txt";
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Leia a string.\n");
+        return 1;
+    }
+
+    // Ler a string do arquivo
+    char original_string[1000];
+    fgets(original_string, sizeof(original_string), file);
+
+    // Determinar o tamanho da string de forma recursiva
+    int string_length = recursive_length(original_string);
+
+    // Fechar o arquivo de leitura
+    fclose(file);
+
+    // Abrir o arquivo no modo de escrita
+    file = fopen(filename, "a");
+
+    if (file == NULL) {
+        printf("Não foi possível abrir o arquivo para escrita.\n");
+        return 1;
+    }
+
+    // Armazenar o tamanho da string no arquivo original
+    fprintf(file, "\nTamanho da string: %d", string_length);
+
+    // Fechar o arquivo de escrita
+    fclose(file);
+
+    printf("Tamanho da string: %d\n", string_length);
+
+    return 0;
+}
