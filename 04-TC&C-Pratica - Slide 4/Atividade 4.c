6a07123c6ebd9b68f00dfc5f1597989001c6b5b0
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2023-10-27 16:46:04 -0300

    Delete 04-TC&C-Pratica - Slide 4 directory

--- 04-TC&C-Pratica - Slide 4/Atividade 4.c
+++
@@ -1,8 +0,0 @@
-result = set([(0, 1)])
-for n in range(1, i + 1):
-new_elements = [(j, n + 1) for j in range(n + 1)]
-result.update(new_elements)
-return result
-i = int(input("Informe o valor de i: "))
-result = MQ(i)
-print("Conjunto MQ para i =", i, ":", result)
