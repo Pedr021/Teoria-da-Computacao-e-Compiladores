b67ee54e1361d7a258c580453931c159c7486636
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2023-10-27 21:33:25 -0300

    Update Atividade 3.c

--- 04-TC&C-Pratica - Slide 4/04-TC$C-PRATICA SLIDE 3/Atividade 3.c
+++ 04-TC&C-Pratica - Slide 4/04-TC$C-PRATICA SLIDE 3/Atividade 3.c
@@ -1,10 +1,32 @@
-def MQ(i):
-if i == 0:
-return set([(0, 1)])
-else:
-previous_MQ = MQ(i - 1)
-new_elements = [(j, i + 1) for j in range(i + 1)]
-return previous_MQ.union(new_elements)
-i = int(input("Informe o valor de i: "))
-result = MQ(i)
-print("Conjunto MQ para i =", i, ":", result)
+#include <stdio.h>
+
+void MQ(int n, int m) {
+    printf("[%d, %d] ", n, m);
+}
+
+void generateMQ(int i, int n, int m) {
+    if (i == 0) {
+        MQ(n, m);
+    } else {
+        generateMQ(i - 1, n, m + 1);
+        for (int j = 0; j <= n; j++) {
+            MQ(j, m);
+        }
+    }
+}
+
+int main() {
+    int i;
+    printf("Informe o valor de 'i': ");
+    scanf("%d", &i);
+
+    if (i < 0) {
+        printf("O valor de 'i' deve ser não negativo.\n");
+    } else {
+        printf("Conjunto MQ%d: ", i);
+        generateMQ(i, 0, 1);
+        printf("\n");
+    }
+
+    return 0;
+}
